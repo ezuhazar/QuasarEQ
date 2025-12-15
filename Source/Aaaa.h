adbeb07d2f2b98172c088ab7b9e5973a8013651c
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-15 09:19:03 +0900

    edit Aaaa.h

--- Source/Aaaa.h
+++ Source/Aaaa.h
@@ -1,29 +1,34 @@
 #pragma once
 
-template <typename FloatType>
-struct MathConstants
-{
-    static constexpr FloatType tau = static_cast<FloatType> (6.283185307179586476L);
-};
+#include <cmath>
 
-template <typename FloatType>
-static FloatType ncos (size_t order, size_t i, size_t size) noexcept
+namespace DSPMath
 {
-    if (size <= 1)
-    {
-        return static_cast<FloatType>(1);
-    }
-    return std::cos (static_cast<FloatType>(order) * static_cast<FloatType>(i) * MathConstants<FloatType>::tau / static_cast<FloatType> (size - 1));
+    constexpr long double tau_ld = 6.283185307179586476L;
 }
 
 template <typename FloatType>
-void calculateBlackmanHarris (FloatType* samples, size_t size) noexcept
+static FloatType getCosineTerm (const size_t harmonicOrder, const size_t i, const size_t windowSize) noexcept
 {
-    for (size_t i = 0; i < size; ++i)
+    if (windowSize <= 1)
     {
-        auto cos1 = ncos<FloatType>(1, i, size);
-        auto cos2 = ncos<FloatType>(2, i, size);
-        auto cos3 = ncos<FloatType>(3, i, size);
-        samples[i] = static_cast<FloatType> (0.35875 - 0.48829 * cos1 + 0.14128 * cos2 - 0.01168 * cos3);
+        return static_cast<FloatType>(1);
+    }
+    return std::cos (static_cast<FloatType>(harmonicOrder) * static_cast<FloatType>(i) * static_cast<FloatType>(DSPMath::tau_ld) / static_cast<FloatType> (windowSize - 1));
+}
+
+template <typename FloatType>
+void calculateBlackmanHarris (FloatType* outputData, const size_t numSamples) noexcept
+{
+    const FloatType a0 = static_cast<FloatType>(0.35875L);
+    const FloatType a1 = static_cast<FloatType>(0.48829L);
+    const FloatType a2 = static_cast<FloatType>(0.14128L);
+    const FloatType a3 = static_cast<FloatType>(0.01168L);
+    for (size_t i = 0; i < numSamples; ++i)
+    {
+        const auto cos1 = getCosineTerm<FloatType>(1, i, numSamples);
+        const auto cos2 = getCosineTerm<FloatType>(2, i, numSamples);
+        const auto cos3 = getCosineTerm<FloatType>(3, i, numSamples);
+        outputData[i] = a0 - a1 * cos1 + a2 * cos2 - a3 * cos3;
     }
 }
--- Source/QuasarHeader.h
+++ Source/QuasarHeader.h
@@ -2,6 +2,7 @@
 
 #include <cstdint>
 #include <JuceHeader.h>
+#include "Aaaa.h"
 
 namespace quasar
 {
