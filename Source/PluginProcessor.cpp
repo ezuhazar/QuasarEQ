03d9cf3570eced57d3b75895efcad48f3114f1d8
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-16 10:23:47 +0900

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -181,7 +181,7 @@ juce::AudioProcessorValueTreeState::ParameterLayout QuasarEQAudioProcessor::crea
     juce::AudioProcessorValueTreeState::ParameterLayout layout;
     layout.add(std::make_unique<juce::AudioParameterFloat>("outGain", "Out Gain", juce::NormalisableRange<float>(-24.0f, 24.0f, 0.001f), 0.0f, "dB"));
     layout.add(std::make_unique<juce::AudioParameterBool>("bypass", "Bypass", false));
-    const std::vector<float> initialFrequencies = {
+    const std::array<float, NUM_BANDS> initialFrequencies = {
         50.0f, 100.0f, 200.0f, 500.0f, 1000.0f, 2000.0f, 5000.0f, 10000.0f
     };
     for (int i = 0; i < NUM_BANDS; ++i)
