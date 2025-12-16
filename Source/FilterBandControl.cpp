52843e260a1ed38611b0f14903023a8ffde61b3f
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-16 14:41:29 +0900

    Update FilterBandControl.cpp

--- Source/FilterBandControl.cpp
+++ Source/FilterBandControl.cpp
@@ -4,40 +4,29 @@
 FilterBandControl::FilterBandControl(juce::AudioProcessorValueTreeState& apvts, int bandIndex)
 {
     juce::String index = juce::String(bandIndex + 1);
-
     freqSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
     freqSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 20);
     addAndMakeVisible(freqSlider);
-    freqAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
-        apvts, "Freq" + index, freqSlider);
-
+    freqAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, "Freq" + index, freqSlider);
     gainSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
     gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 20);
     addAndMakeVisible(gainSlider);
-    gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
-        apvts, "Gain" + index, gainSlider);
-
+    gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, "Gain" + index, gainSlider);
     qSlider.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
     qSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 20);
     addAndMakeVisible(qSlider);
-    qAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
-        apvts, "Q" + index, qSlider);
-
+    qAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(apvts, "Q" + index, qSlider);
     typeComboBox.addItem("HighPass", 1);
     typeComboBox.addItem("HighShelf", 2);
     typeComboBox.addItem("LowPass", 3);
     typeComboBox.addItem("LowShelf", 4);
     typeComboBox.addItem("PeakFilter", 5);
-
     addAndMakeVisible(typeComboBox);
-    typeAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(
-        apvts, "Type" + index, typeComboBox);
+    typeAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ComboBoxAttachment>(apvts, "Type" + index, typeComboBox);
 }
-
 FilterBandControl::~FilterBandControl()
 {
 }
-
 void FilterBandControl::resized()
 {
     auto bounds = getLocalBounds().reduced(5);
