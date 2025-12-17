c31da9d5c95429167982485229e9d7aef6ac7bc0
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-18 08:55:27 +0900

    Update FilterBandControl.h

--- Source/FilterBandControl.h
+++ Source/FilterBandControl.h
@@ -2,6 +2,32 @@
 
 #include <JuceHeader.h>
 
+class CustomLNF: public juce::LookAndFeel_V4
+{
+public:
+    void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
+        float sliderPosProportional, float rotaryStartAngle,
+        float rotaryEndAngle, juce::Slider& slider) override
+    {
+        auto bounds = juce::Rectangle<float>(x, y, width, height).reduced(2.0f);
+        auto radius = juce::jmin(bounds.getWidth(), bounds.getHeight()) / 2.0f;
+        auto toAngle = rotaryStartAngle + sliderPosProportional * (rotaryEndAngle - rotaryStartAngle);
+        auto lineThickness = 2.0f;
+        auto arcRadius = radius - lineThickness * 0.5f;
+        g.setColour(juce::Colours::darkgrey);
+        g.fillEllipse(bounds.getCentreX() - radius, bounds.getCentreY() - radius, radius * 2.0f, radius * 2.0f);
+        g.setColour(juce::Colours::cyan);
+        g.drawEllipse(bounds.getCentreX() - radius, bounds.getCentreY() - radius, radius * 2.0f, radius * 2.0f, 1.0f);
+        juce::Path p;
+        auto pointerLength = radius * 0.8f;
+        auto pointerThickness = 3.0f;
+        p.addRectangle(-pointerThickness * 0.5f, -radius, pointerThickness, pointerLength);
+        p.applyTransform(juce::AffineTransform::rotation(toAngle).translated(bounds.getCentreX(), bounds.getCentreY()));
+        g.setColour(juce::Colours::white);
+        g.fillPath(p);
+    }
+};
+
 class FilterBandControl: public juce::Component
 {
 public:
@@ -9,6 +35,7 @@ public:
     ~FilterBandControl() override;
     void resized() override;
 private:
+    CustomLNF customLNF;
     juce::Slider freqSlider;
     juce::Slider gainSlider;
     juce::Slider qSlider;
