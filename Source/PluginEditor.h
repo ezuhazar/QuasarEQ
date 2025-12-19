a242978405759e5a166ba62207628679a6beb4ba
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-19 11:19:32 +0900

    refactor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -202,11 +202,63 @@ public:
 class QuasarEQAudioProcessorEditor: public juce::AudioProcessorEditor, public juce::ChangeListener
 {
 public:
-    QuasarEQAudioProcessorEditor(QuasarEQAudioProcessor&);
-    ~QuasarEQAudioProcessorEditor();
-    void paint(juce::Graphics&) override;
-    void resized() override;
-    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+    QuasarEQAudioProcessorEditor(QuasarEQAudioProcessor& p): AudioProcessorEditor(&p), audioProcessor(p), visualizerComponent(p), pluginInfoComponent()
+    {
+        audioProcessor.addChangeListener(this);
+        for (int i = 0; i < 8; ++i)
+        {
+            bandControls.push_back(std::make_unique<FilterBandControl>(audioProcessor.apvts, i));
+            addAndMakeVisible(*bandControls.back());
+        }
+        addAndMakeVisible(visualizerComponent);
+        addAndMakeVisible(pluginInfoComponent);
+        gainSlider.setLookAndFeel(&customLNF);
+        addAndMakeVisible(gainSlider);
+        addAndMakeVisible(bypathButton);
+        outGainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvts, "outGain", gainSlider);
+        bypassAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.apvts, "bypass", bypathButton);
+        setSize(664, 650);
+    };
+    ~QuasarEQAudioProcessorEditor()
+    {
+        audioProcessor.removeChangeListener(this);
+    };
+    void paint(juce::Graphics& g) override
+    {
+        g.fillAll(BACKGROUND_COLOR);
+    };
+    void resized() override
+    {
+        const int margin = 6;
+        const int topSectionHeight = 42;
+        const int midSectionHeight = 290;
+        const int botSectionHeight = 320;
+        juce::Rectangle<int> mainArea = getLocalBounds().reduced(margin);
+        juce::Rectangle<int> top = mainArea.removeFromTop(topSectionHeight).reduced(margin);
+        juce::Rectangle<int> mid = mainArea.removeFromTop(midSectionHeight).reduced(margin);
+        juce::Rectangle<int> bot = mainArea.removeFromTop(botSectionHeight).reduced(margin);
+        const int sideSize = top.getHeight();
+        bypathButton.setBounds(top.removeFromLeft(sideSize).reduced(margin));
+        pluginInfoComponent.setBounds(top.reduced(margin));
+        visualizerComponent.setBounds(mid);
+        gainSlider.setBounds(bot.removeFromRight(20 * 3).reduced(margin));
+        const int bandWidth = bot.getWidth() / audioProcessor.NUM_BANDS;
+        for (int i = 0; i < audioProcessor.NUM_BANDS; ++i)
+        {
+            if (bandControls[i])
+            {
+                bandControls[i]->setBounds(bot.removeFromLeft(bandWidth));
+            }
+        }
+    };
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override
+    {
+
+        if (source == &audioProcessor)
+        {
+            visualizerComponent.parametersNeedUpdate = true;
+        }
+    };
 private:
     QuasarEQAudioProcessor& audioProcessor;
     const juce::Colour BACKGROUND_COLOR = juce::Colour(juce::uint8(40), juce::uint8(42), juce::uint8(50));
