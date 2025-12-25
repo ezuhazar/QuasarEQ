37dcb2e3e6fabf5f77b5933d28f849041715930e
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-25 23:46:53 +0900

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -355,7 +355,7 @@ private:
         auto bounds = getCurveArea().toFloat();
         const float minDb = -24.0f;
         const float maxDb = 24.0f;
-        const float tolerance = 7.0f;
+        const float tolerance = 7.75f;
         for (int i = 0; i < audioProcessor.NUM_BANDS; ++i)
         {
             juce::String index = juce::String(i + 1);
