168d2cd273e07e3ed2fa9a1c91b5603e61bacb1e
HeavyXenon <medaka1cocoa1@gmail.com>
2025-12-25 16:13:45 +0900

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,7 +1,5 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-
-
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter() { return new QuasarEQAudioProcessor(); }
 juce::AudioProcessorEditor* QuasarEQAudioProcessor::createEditor() { return new QuasarEQAudioProcessorEditor(*this); }
